b5d650b3c7317422562e9b9a1605105839521d91
Курцев Петр Анатольевич <kurtsev_petr@mail.ru>
2022-03-18 10:13:55 +0300

    added ElasticSearch

---
+++ src/CG4.Impl.ElasticSearch/IElasticsearchConnectionSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace CG4.Impl.ElasticSearch
+{
+    /// <summary>
+    /// Elasticsearch connection settings
+    /// </summary>
+    public interface IElasticsearchConnectionSettings
+    {
+        /// <summary>
+        /// Elastic search nodes
+        /// </summary>
+        IEnumerable<string> Nodes { get; set; }
+    }
+}
---
+++ src/CG4.Impl.ElasticSearch/Impl/ElasticsearchConnectionFactory.cs
@@ -0,0 +1,50 @@
+﻿using CG4.Extensions;
+using Elasticsearch.Net;
+using Nest;
+
+namespace CG4.Impl.ElasticSearch.Impl
+{
+    /// <summary>
+    /// Elasticsearch client factory
+    /// </summary>
+    public class ElasticsearchConnectionFactory : IElasticsearchConnectionFactory
+    {
+        private readonly ConnectionSettings _connectionSettings;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ElasticsearchConnectionFactory"/> class.
+        /// </summary>
+        /// <param name="setting"></param>
+        public ElasticsearchConnectionFactory(IElasticsearchConnectionSettings setting)
+        {
+            if (setting == null)
+            {
+                throw new ArgumentNullException(nameof(setting));
+            }
+
+            if (setting.Nodes.NullOrEmpty())
+            {
+                throw new ArgumentOutOfRangeException(nameof(setting), "Should be used one node at least");
+            }
+
+            var staticConnectionPool = new StaticConnectionPool(setting.Nodes.Select(x => new Uri(x)));
+            _connectionSettings = new ConnectionSettings(staticConnectionPool);
+        }
+
+        /// <summary>
+        /// Create new client instance
+        /// </summary>
+        public IElasticClient CreateClient()
+        {
+            return CreateClientAsync().Result;
+        }
+
+        /// <summary>
+        /// Create new client instance
+        /// </summary>
+        public Task<IElasticClient> CreateClientAsync()
+        {
+            return Task.FromResult<IElasticClient>(new ElasticClient(_connectionSettings));
+        }
+    }
+}
