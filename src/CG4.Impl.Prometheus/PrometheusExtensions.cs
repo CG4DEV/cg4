c780b89ab685b7f3cba8c8967e6d75d66a46f249
Курцев Петр Анатольевич <kurtsev_petr@mail.ru>
2022-03-21 10:50:24 +0300

    Added prometheus

---
+++ src/CG4.Impl.Prometheus/PrometheusExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Prometheus;
+
+namespace CG4.Impl.Prometheus
+{
+    public static class PrometheusExtensions
+    {
+        public static IDisposable MeasureDuration<T>(this T @this, params string[] labels)
+            where T : Collector
+        {
+            switch (@this)
+            {
+                case Histogram histogram: return new HistogramWatcher(histogram, labels);
+                default: throw new NotSupportedException(typeof(T).Name);
+            }
+        }
+    }
+}
