42e94c476a9006537ed651506ed9d09191096b16
sergius1che <sergius1che@gmail.com>
2022-08-15 12:00:11 +0300

    Update IAppCrudService and implement it

--- src/CG4.Impl.Dapper/Crud/IAppCrudService.cs
+++ src/CG4.Impl.Dapper/Crud/IAppCrudService.cs
@@ -13,27 +13,34 @@ namespace CG4.Impl.Dapper.Crud
         Task<IEnumerable<T>> GetAllAsync<T>(string sql, object param = null, IDbConnection connection = null, IDbTransaction transaction = null)
             where T : class;
 
-        Task<T> GetAsync<T>(IEntity context, Expression<Action<IClassSqlOptions<T>>> predicate = null, IDbConnection connection = null, IDbTransaction transaction = null)
+        Task<T> GetAsync<T>(long id, IDbConnection connection = null, IDbTransaction transaction = null)
             where T : class, IEntityBase, new();
+
+        Task<T> GetAsync<T>(Expression<Action<IClassSqlOptions<T>>> predicate, IDbConnection connection = null, IDbTransaction transaction = null)
+            where T : class, IEntityBase, new();
+
+        Task<TResult> GetAsync<TEntity, TResult>(long id, IDbConnection connection = null, IDbTransaction transaction = null)
+            where TEntity : class, IEntityBase, new()
+            where TResult : class, new();
+
+        Task<TResult> GetAsync<TEntity, TResult>(Expression<Action<IClassSqlOptions<TEntity>>> predicate, IDbConnection connection = null, IDbTransaction transaction = null)
+            where TEntity : class, IEntityBase, new()
+            where TResult : class, new();
 
         Task<IEnumerable<T>> GetAllAsync<T>(Expression<Action<IClassSqlOptions<T>>> predicate = null, IDbConnection connection = null, IDbTransaction transaction = null)
             where T : class, IEntityBase, new();
-
-        Task<TResult> GetAsync<TEntity, TResult>(IEntity context, Expression<Action<IClassSqlOptions<TEntity>>> predicate = null, IDbConnection connection = null, IDbTransaction transaction = null)
-            where TEntity : class, IEntityBase, new()
-            where TResult : class, new();
 
         Task<IEnumerable<TResult>> GetAllAsync<TEntity, TResult>(Expression<Action<IClassSqlOptions<TEntity>>> predicate = null, IDbConnection connection = null, IDbTransaction transaction = null)
             where TEntity : class, IEntityBase, new()
             where TResult : class, new();
 
-        Task<T> CreateAsync<T>(object context, IDbConnection connection = null, IDbTransaction transaction = null)
+        Task<T> CreateAsync<T>(T entity, IDbConnection connection = null, IDbTransaction transaction = null)
             where T : class, IEntityBase, new();
 
-        Task<EntityContext> UpdateAsync<T>(IEntity context, IDbConnection connection = null, IDbTransaction transaction = null)
+        Task<T> UpdateAsync<T>(T entity, IDbConnection connection = null, IDbTransaction transaction = null)
             where T : class, IEntityBase, new();
 
-        Task<EntityContext> DeleteAsync<T>(IEntity context, IDbConnection connection = null, IDbTransaction transaction = null)
+        Task DeleteAsync<T>(long id, IDbConnection connection = null, IDbTransaction transaction = null)
             where T : class, IEntityBase, new();
     }
 }
