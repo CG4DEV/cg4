4ebfbee03719dec2cfe1cb94ea7a9e5f7d8bf211
VladislavMerkulov <vladislav.merkulov@itl.team>
2022-12-20 15:05:25 +0300

    Add template for Consumer App

---
+++ src/CG4.Templ.ConsumerApp/src/ProjectName.Migrations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProjectName.Migrations
+{
+    class Program
+    {
+        private const string ENVIRONMENT_VARIABLE_NAME = "ASPNETCORE_ENVIRONMENT";
+        private const string DATABASE_NAME = "ProjectName";
+        
+        static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static IServiceProvider CreateServices()
+        {
+            var env = Environment.GetEnvironmentVariable(ENVIRONMENT_VARIABLE_NAME);
+            var config = new ConfigurationBuilder()
+                .AddJsonFile($"Configs/connectionStrings.{env}.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            return new ServiceCollection()
+                .AddSingleton<IConfiguration>(config)
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddPostgres10_0()
+                    .WithGlobalConnectionString(x =>
+                        x.GetRequiredService<IConfiguration>().GetConnectionString(DATABASE_NAME))
+                    .WithGlobalCommandTimeout(TimeSpan.FromMinutes(5))
+                    .ScanIn(typeof(Program).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp();
+        }
+    }
+}
